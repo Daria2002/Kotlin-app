908533ca5c72fd127c3c20859cc550cd52fc44a3
Daria2002 <daria.matkovic@fer.hr>
2020-09-20 23:34:26 +0200

    Add some impl, it's running

--- base/src/main/java/com/example/base/fragment/SignInFragment.kt
+++ base/src/main/java/com/example/base/fragment/SignInFragment.kt
@@ -2,6 +2,8 @@ package com.example.base.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +12,14 @@ import androidx.fragment.app.Fragment
 import com.example.base.R
 import com.example.base.helper.*
 import com.example.base.model.Worker
+import com.example.base.widget.TextWatcherAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SignInFragment : Fragment() {
     private var firstNameView: EditText? = null
     private var lastNameView: EditText? = null
     private var worker: Worker? = null
+    private var doneFab: FloatingActionButton? = null
     private val edit by lazy { arguments?.getBoolean(ARG_EDIT, false) ?: false }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -77,8 +82,54 @@ class SignInFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        firstNameView = view.findViewById<EditText>(R.id.first_name)
+        lastNameView = view.findViewById<EditText>(R.id.last_name)
+        doneFab = view.findViewById<FloatingActionButton>(R.id.done)
+
+        if(edit || (worker != null && worker!!.valid())) {
+            initContentViews()
+            initContents()
+        }
+        hideEmptyView()
         super.onViewCreated(view, savedInstanceState)
     }
+
+    private fun hideEmptyView() {
+        view?.run {
+            findViewById<View>(R.id.empty).visibility = View.GONE
+            findViewById<View>(R.id.content).visibility = View.VISIBLE
+        }
+    }
+
+    private fun initContents() {
+        worker?.run {
+            valid().let {
+                firstNameView?.setText(firstName)
+                lastNameView?.setText(lastName)
+            }
+        }
+    }
+
+    private fun initContentViews() {
+        val textWatcher = object : TextWatcher by TextWatcherAdapter {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                // hiding the floating action button if text is empty
+                if(s.isEmpty()) doneFab?.hide()
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                if(isInputDataValid()) doneFab?.show()
+            }
+        }
+        firstNameView?.addTextChangedListener(textWatcher)
+        lastNameView?.addTextChangedListener(textWatcher)
+        doneFab?.setOnClickListener {
+
+        }
+    }
+
+    private fun isInputDataValid() = firstNameView?.text?.isNotEmpty() == true &&
+            lastNameView?.text?.isNotEmpty() == true
 
     companion object {
         private const val ARG_EDIT = "EDIT"
