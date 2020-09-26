da2abeccf3ca799df0ad9e1ecc3fbe2622a05d6c
Daria2002 <daria.matkovic@fer.hr>
2020-09-26 21:36:00 +0200

    Add some tminor changes

--- app/src/main/java/com/example/w4e/mvvm/view/ui/LoginActivity.kt
+++ app/src/main/java/com/example/w4e/mvvm/view/ui/LoginActivity.kt
@@ -6,8 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.w4e.R
 
 class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar!!.hide()
     }
 }
