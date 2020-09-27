d923c7fafd700ea93430d5fad06eb7a55fa31453
Daria2002 <daria.matkovic@fer.hr>
2020-09-27 22:21:31 +0200

    app name is displayed

--- app/src/main/java/com/example/w4e/mvvm/view/ui/LoginActivity.kt
+++ app/src/main/java/com/example/w4e/mvvm/view/ui/LoginActivity.kt
@@ -3,12 +3,13 @@ package com.example.w4e.mvvm.view.ui
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.w4e.R
+import com.example.w4e.mvvm.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        supportActionBar!!.hide()
+        println("bok")
+        // supportActionBar!!.hide()
     }
 }
