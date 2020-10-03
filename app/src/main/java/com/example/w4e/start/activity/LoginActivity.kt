244ca85e7f7fc0226e8932a875e7724611def4a5
Daria2002 <daria.matkovic@fer.hr>
2020-10-03 08:59:34 +0200

    Add some code for validating the input text fields

--- app/src/main/java/com/example/w4e/start/activity/LoginActivity.kt
+++ app/src/main/java/com/example/w4e/start/activity/LoginActivity.kt
@@ -8,6 +8,9 @@ import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.widget.NestedScrollView
 import com.example.w4e.start.R
+import com.example.w4e.start.helper.DatabaseHelper
+import com.example.w4e.start.helper.InputValidation
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -20,10 +23,8 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var textInputEditTextPassword: TextInputEditText
     private lateinit var appCompatButtonLogin: AppCompatButton
     private lateinit var textViewLinkRegister: AppCompatTextView
-    /*
     private lateinit var inputValidation: InputValidation
     private lateinit var databaseHelper: DatabaseHelper
-    */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,7 +60,8 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
      * This method initialize objects
      */
     private fun initObjects() {
-
+        databaseHelper = DatabaseHelper(activity)
+        inputValidation = InputValidation(activity)
     }
 
     /**
@@ -76,7 +78,35 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    /**
+     * This method validates the input text fields and verify login credentials from SQLite
+     */
     private fun verifyFromSQLite() {
-        TODO("Not yet implemented")
+        if(!inputValidation!!.isInputEditTextFilled(textInputEditTextEmail!!, textInputLayoutEmail!!, getString(R.string.error_message_email))) {
+            return
+        }
+        if(!inputValidation!!.isInputEditTextEmail(textInputEditTextEmail!!, textInputLayoutEmail!!, getString(R.string.error_message_email))) {
+            return
+        }
+        if(!inputValidation!!.isInputEditTextFilled(textInputEditTextPassword!!, textInputLayoutPassword!!, getString(R.string.error_message_email))) {
+            return
+        }
+        if(databaseHelper!!.checkUser(textInputEditTextEmail!!.text.toString().trim {it <= ' '}, textInputEditTextPassword!!.text.toString().trim{ it <= ' '})) {
+            val accountsIntent = Intent(activity, CategoryListActivity::class.java)
+            accountsIntent.putExtra("EMAIL", textInputEditTextEmail!!.text.toString().trim {it <= ' '})
+            emptyInputEditText()
+            startActivity(accountsIntent)
+        } else {
+            // Snack Bar to show success message that record is wrong
+            Snackbar.make(nestedScrollView!!, getString(R.string.error_valid_email_password), Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    /**
+     * This method clears all input edit text
+     */
+    private fun emptyInputEditText() {
+        textInputEditTextEmail!!.text = null
+        textInputEditTextPassword!!.text = null
     }
 }
