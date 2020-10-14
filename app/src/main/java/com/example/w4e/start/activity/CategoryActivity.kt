f363f3732bdf266e743925f376ab06c5d169fb66
Daria2002 <daria.matkovic@fer.hr>
2020-10-14 09:22:32 +0200

    Add fix in category selection and start working on activity with posts

---
+++ app/src/main/java/com/example/w4e/start/activity/CategoryActivity.kt
@@ -0,0 +1,21 @@
+package com.example.w4e.start.activity
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * This activity is used for displaying posts of selected category
+ */
+class CategoryActivity: AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    /**
+     * Open post for more info.
+     */
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+}
