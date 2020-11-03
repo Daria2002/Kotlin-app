fa664a73928f407b8ac0b7cdde95531f3eac39c4
Daria2002 <daria.matkovic@fer.hr>
2020-11-03 09:14:49 +0100

    Add tabs in Ususer profile activity

--- app/src/main/java/com/example/w4e/start/activity/UserProfileActivity.kt
+++ app/src/main/java/com/example/w4e/start/activity/UserProfileActivity.kt
@@ -4,26 +4,48 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.viewpager.widget.ViewPager
 import com.example.w4e.start.R
+import com.example.w4e.start.adapter.UserProfileAdapter
+import com.google.android.material.tabs.TabLayout
 
 class UserProfileActivity: AppCompatActivity(), View.OnClickListener {
     private lateinit var userName: String
-    private lateinit var userNameText: AppCompatTextView
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager: ViewPager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         userName = intent.getStringExtra("USER_NAME").toString()
         setContentView(R.layout.activity_user_profile)
-        supportActionBar!!.hide()
         initViews()
+        initListeners()
         initObjects()
     }
 
+    private fun initListeners() {
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager.currentItem = tab.position
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+    }
+
     private fun initObjects() {
-        userNameText.text = userName
+        tabLayout.addTab(tabLayout.newTab().setText("Review"))
+        tabLayout.addTab(tabLayout.newTab().setText("CV and Experience"))
+        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+        val adapter = UserProfileAdapter(this, supportFragmentManager,
+            tabLayout.tabCount)
+        viewPager.adapter = adapter
     }
 
     private fun initViews() {
-        userNameText = findViewById(R.id.userName) as AppCompatTextView
+        title = userName + "'s Profile"
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager = findViewById(R.id.viewPager)
     }
 
     override fun onClick(v: View?) {
---
+++ app/src/main/java/com/example/w4e/start/adapter/UserProfileAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.w4e.start.adapter
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.w4e.start.fragment.CVAndExperience
+import com.example.w4e.start.fragment.Review
+
+@Suppress("DEPRECATION")
+internal class UserProfileAdapter(
+    var context: Context,
+    fm: FragmentManager,
+    var totalTabs: Int
+) :
+    FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                CVAndExperience()
+            }
+            1 -> {
+                Review()
+            }
+            else -> getItem(position)
+        }
+    }
+    override fun getCount(): Int {
+        return totalTabs
+    }
+}
